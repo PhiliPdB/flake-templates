1ba515a0952c32f8ea34008973de0c393afb31de
PhiliPdB <phlpdbrn@gmail.com>
2026-05-23 21:13:56 +0200

    Add zig shell

---
+++ zig.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    {
+      flake-parts,
+      nixpkgs,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.zig = pkgs.mkShell {
+            name = "Zig shell";
+
+            packages = with pkgs; [
+              zig
+              zls
+            ];
+          };
+        };
+    };
+}
